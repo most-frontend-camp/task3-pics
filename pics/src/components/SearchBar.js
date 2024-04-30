ce34218399fb172a59ca8bca3acc95ca86496a9c
Beibarys Learner <beibarys.learner@gmail.com>
2024-05-01 02:51:09 +0500

    css styling

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,4 +1,6 @@
+import './SearchBar.css';
 import {useState} from "react";
+
 
 function SearchBar({onSubmit}) {
     const [term, setTerm] = useState("");
@@ -13,8 +15,9 @@ function SearchBar({onSubmit}) {
     };
 
     return (
-        <div>
+        <div className="search-bar">
             <form onSubmit={handleFormSubmit}>
+                <label>Напишите в поисковике что вы ищите</label>
                 <input value={term} onChange={handleChange}/>
             </form>
         </div>
